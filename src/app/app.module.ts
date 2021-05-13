e00748089277eeea61bd41e94bbfcab6d51aa432
xRUsCHx <xruschx@gmail.com>
2021-05-13 22:51:44 +0200

    input component has been added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search/search-bar/search-bar.component';
+import { InputComponent } from './common/input/input.component';
+import { ButtonComponent } from './common/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    InputComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
