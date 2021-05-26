9bcf19371c0fa4828730c3e12738512fed8b17a6
Andrzej Herman <ahm@kmdpoland.pl>
2021-05-26 22:40:26 +0200

    #2: router and search component have been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/search', pathMatch: 'full'},
+  {path: 'search', component: SearchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
